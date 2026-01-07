0e5a3402b72035268efe4003b0811e5ab8299551
nhantran11325-netizen <nhantran11325@gmail.com>
2026-01-07 13:32:57 +0700

    Update checkkey.lua

--- checkkey.lua
+++ checkkey.lua
@@ -1,4 +1,4 @@
-local WebAppUrl = "LINK_WEB_APP_MỚI_CỦA_BẠN"
+local WebAppUrl = "https://script.google.com/macros/s/AKfycbx7BbJAxuCGGYl1YPpMfl0T9_mfdbp1vryt3zivfV5KfriQiC0pKJVHLf44VNPthCtY/exec"
 
 local function Verify()
     if getgenv().Key == "" then 
